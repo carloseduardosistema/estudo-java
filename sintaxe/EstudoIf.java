4e682a717f980a9b3e4c1a5347b2c005c3194667
Carlos Eduardo <carloseduardosistema@gmail.com>
2024-11-10 21:49:31 -0300

    Iniciado estudo do if

---
+++ sintaxe/EstudoIf.java
@@ -0,0 +1,8 @@
+package sintaxe;
+
+public class EstudoIf {
+    public static void main (String args[]) {
+        System.out.println("Estudando IF");
+    }
+    
+}
--- sintaxe/Variaveis.java
+++ sintaxe/Variaveis.java
@@ -2,6 +2,6 @@ package sintaxe;
 
 class Variaveis {
     public static void main (String args[]) {
-        System.out.println("Inicio da classe Variaveis");
+        System.out.println("Estudando Variaveis");
     }
 }
